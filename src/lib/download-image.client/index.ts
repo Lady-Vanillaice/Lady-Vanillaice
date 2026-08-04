e19c867154e30265a23d8a53a02a3eac05219120
Lady-Vanillaice <herzblutmadl3@gmail.com>
2026-08-04 22:14:04 +0200

    Load text-anchored merge button helper

--- src/lib/download-image.client/index.ts
+++ src/lib/download-image.client/index.ts
@@ -2,3 +2,4 @@ export { saveCanvasAsPng } from "./core";
 export { exportCalendarImage } from "./calendar-image-export";
 import "./admin-export-field-visibility";
 import "./admin-calendar-final-fixes";
+import "./merge-button-visible-text-anchor";
