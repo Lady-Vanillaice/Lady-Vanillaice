5fac2b860a88848dcb687ad5af9f9a510601ff2f
Lady-Vanillaice <herzblutmadl3@gmail.com>
2026-08-06 09:51:02 +0200

    Initialize duo single-only editor in admin calendar

--- src/lib/download-image.client/index.ts
+++ src/lib/download-image.client/index.ts
@@ -2,3 +2,6 @@ export { saveCanvasAsPng } from "./core";
 export { exportCalendarImage } from "./calendar-image-export";
 import "./admin-export-field-visibility";
 import "./admin-calendar-final-fixes";
+import { installAdminDuoSingleOnlyEditor } from "./admin-duo-single-only-editor";
+
+installAdminDuoSingleOnlyEditor();
