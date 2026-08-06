90807835b6278588bf226d6ed3e3e1ba5de71529
Lady-Vanillaice <herzblutmadl3@gmail.com>
2026-08-06 11:54:40 +0200

    Remove obsolete manual booking DOM enhancement

--- src/lib/download-image.client/index.ts
+++ src/lib/download-image.client/index.ts
@@ -3,7 +3,5 @@ export { exportCalendarImage } from "./calendar-image-export";
 import "./admin-export-field-visibility";
 import "./admin-calendar-final-fixes";
 import { installAdminDuoSingleOnlyEditor } from "./admin-duo-single-only-editor";
-import { installAdminManualBookingEmailField } from "./admin-manual-booking-email-field";
 
 installAdminDuoSingleOnlyEditor();
-installAdminManualBookingEmailField();
